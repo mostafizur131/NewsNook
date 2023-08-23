f272c8f64859db7173e9aa0c3734561eb339b65b
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-08-23 23:21:37 +0600

    added headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <Link href="/" className="flex items-center p-2">
           NewsNook
         </Link>
-        <ul className="items-stretch hidden space-x-3 md:flex">
+        <ul className="items-stretch space-x-3 flex flex-wrap">
           <li className="flex">
             <Link
               href="/"
@@ -42,22 +42,6 @@ const Header = () => {
             </Link>
           </li>
         </ul>
-        <button className="flex justify-end p-4 md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
-        </button>
       </div>
     </header>
   );
